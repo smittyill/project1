4e98c337179a485e0f3713430c368b3b089e7878
smittyill <smithtodda@johndeere.com>
2021-08-02 14:45:09 -0500

    another version

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ Jeopardy is made with HTML, CSS and JavaScript
 
 game is simple Q and A.
 
+Remember with Jeopardy all responses are in the form of a question.
+example would be 
+Question:
+The first president
+Answer:
+Who is Gerorge Washington
+Once you have responded to a question click Submit.  It will then tell you if your correct.
+If yoo would like another question, just click the next question button.
+For more information on Jeopardy
--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 
-    const question = ["what you want to do", "2nd question", "3rd question"]
-    const newquestion = document.getElementById('question').value;
-    //items.push(question); 
 
 
-    document.getElementById('submit').addEventListener("click", function( ){
-        const question = ["what you want to do", "2nd question", "3rd question"]
-        const newquestion = document.getElementById('question').value;
-        console.log (newquestion);
 
 
+let answers = [
+    { question: 'A large Ag MFG in the Quad Cities', answer: 'who is John Deere' },
+ { question: 'Who is CEO of John Deere', answer: 'who is John May' },
+ { question: 'Who is the first president of the USA', answer: 'who is George Washington' },
+ { question: 'Who is govenor of Iowa', answer: 'who is Reynolds' },
+  { question: 'Where is John Deere HQ located', answer: 'where is Moline' }
+
+]
+let newquestion = document.getElementById('question');
+
+//items.push(question); 
+
+newquestion.placeholder = answers[0].question;
+console.log(newquestion);
+
+document.getElementById('submit').addEventListener("click", function () {
+   // let question = ["what you want to do", "2nd question", "3rd question"]
+    let newquestion = document.getElementById('question').value;
+    let answer = document.getElementById("answer").value;
+    
+    console.log(answers[index].answer);
+    console.log(answer);
+    if (answer === answers[index].answer) {
+        //do something
+        document.getElementById("response").innerText = "Correct!";
     }
-    )
+    else {
+        //do something else
+        document.getElementById("response").innerText = "Sorry wrong answer!";
+    }
 
+}
+)
+let index=0;
+
+document.getElementById("next_question").addEventListener('click', function () {
+    var text = document.getElementById('question');
+    console.log("hello world");
+    index++;
+    console.log(answers[index].question);
+    document.getElementById("answer").value = "";
+    document.getElementById("response").innerText = "";
+    newquestion.placeholder = answers[index].question;
+})
